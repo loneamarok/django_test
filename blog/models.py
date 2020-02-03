e09afb26654d83a2bd38c6bc784dc171e212bee8
srirrao <srirrao@cisco.com>
2020-02-04 00:03:56 +0530

    after bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=500)
+    datetime = models.DateTimeField()
--- myproject/urls.py
+++ myproject/urls.py
@@ -18,11 +18,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+import job.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('count/', views.count, name="count"),
     path('hello/', views.hello, name="hello"),
     path('about/', views.about, name="about"),
+    path('', job.views.homepage, name="homepage"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
