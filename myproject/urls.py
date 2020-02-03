b9459c58ff9bd0c93b75bfb94738501e09152d50
srirrao <srirrao@cisco.com>
2020-02-03 17:40:31 +0530

    postgres done

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('count/', views.count, name="count"),
     path('hello/', views.hello, name="hello"),
     path('about/', views.about, name="about"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
